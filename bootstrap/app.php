afa5575d819188f4ddab21cb0ef9aa45da1937ea
mcmonde <mcmeliton1889@gmail.com>
2024-10-30 13:07:13 +0800

    added new env variables for setup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        apiPrefix: 'api-your-app-name'
+        apiPrefix: env('APP_PREFIX', 'api')
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->group('log.route',[
+           \App\Http\Middleware\LogRouteMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

165593635662508f4284712c08385a26fec507f5
mcmonde <mcmeliton1889@gmail.com>
2025-03-10 11:14:46 +0800

    added digitalocean filesystam config.

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,14 @@ return [
             'report' => false,
         ],
 
+        'digitalocean' => [
+            'driver' => 's3',
+            'key' => env('DIGITALOCEAN_SPACES_KEY'),
+            'secret' => env('DIGITALOCEAN_SPACES_SECRET'),
+            'endpoint' => env('DIGITALOCEAN_SPACES_ENDPOINT'),
+            'region' => env('DIGITALOCEAN_SPACES_REGION'),
+            'bucket' => env('DIGITALOCEAN_SPACES_BUCKET'),
+        ],
     ],
 
     /*

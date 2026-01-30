701c707e50c6a9002a94c9d7c6a604fea44d6979
mcmonde <mcmeliton1889@gmail.com>
2026-01-30 11:37:52 +0800

    remove header and column selected.

--- app/Http/Controllers/AbilityController.php
+++ app/Http/Controllers/AbilityController.php
@@ -18,13 +18,6 @@ use Illuminate\Http\JsonResponse;
 class AbilityController extends Controller
 {
     protected AbilityRepository $repository;
-    protected array $selected_relation_columns_only = [
-        'abilities'   => ['id','name','title'],
-    ];
-    protected array $headers = [
-        ['text' => 'Unique Name', 'value' => 'abilities_name', 'align' => 'left', 'sortable' => false],
-        ['text' => 'Title', 'value' => 'abilities_title', 'align' => 'left', 'sortable' => false],
-    ];
 
     public function __construct(AbilityRepository $repository)
     {
@@ -34,7 +27,7 @@ class AbilityController extends Controller
     public function index(Index $request): JsonResponse
     {
         $payload = $request->validated();
-        $data = $this->repository->index($payload, $this->selected_relation_columns_only, $this->headers);
+        $data = $this->repository->index($payload);
         return $this->repository->getJsonResponse($data);
     }
 
@@ -47,44 +40,44 @@ class AbilityController extends Controller
     public function store(Store $request): JsonResponse
     {
         $payload = $request->validated();
-        $data = $this->repository->store($payload, $this->selected_relation_columns_only, $this->headers);
+        $data = $this->repository->store($payload);
         return $this->repository->getJsonResponse($data);
     }
 
     public function show(Show $request, $id): JsonResponse
     {
-        $data = $this->repository->show($id, $this->selected_relation_columns_only);
+        $data = $this->repository->show($id);
         return $this->repository->getJsonResponse($data);
     }
 
     public function edit(Edit $request, $id): JsonResponse
     {
-        $data = $this->repository->edit($id, $this->selected_relation_columns_only);
+        $data = $this->repository->edit($id);
         return $this->repository->getJsonResponse($data);
     }
 
     public function update(Update $request, $id): JsonResponse
     {
         $payload = $request->validated();
-        $data = $this->repository->update($payload, $id, $this->selected_relation_columns_only, $this->headers);
+        $data = $this->repository->update($payload, $id,);
         return $this->repository->getJsonResponse($data);
     }
 
     public function softDelete(SoftDelete $request, $id): JsonResponse
     {
-        $data = $this->repository->softDelete($id, $this->selected_relation_columns_only);
+        $data = $this->repository->softDelete($id);
         return $this->repository->getJsonResponse($data);
     }
 
     public function permanentDelete(PermanentDelete $request, $id): JsonResponse
     {
-        $data = $this->repository->permanentDelete($id, $this->selected_relation_columns_only);
+        $data = $this->repository->permanentDelete($id,);
         return $this->repository->getJsonResponse($data);
     }
 
     public function restore(Restore $request, $id): JsonResponse
     {
-        $data = $this->repository->restore($id, $this->selected_relation_columns_only);
+        $data = $this->repository->restore($id);
         return $this->repository->getJsonResponse($data);
     }
 
